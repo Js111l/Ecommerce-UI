028f99d93845c38a1f25ab20c5f74f01abc6e388
Jakub <k.swiercz@protonmail.com>
2024-08-04 18:08:34 +0200

    created basic login UI

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react";
 import { Password } from 'primereact/password';
 import PasswordCredentialsContainer from "./PasswordCredentialsContainer";
+import { Button } from "primereact/button";
 
 const LoginContainer = (props) => {
     //const navigation = useNavigate();
@@ -17,10 +18,72 @@ const LoginContainer = (props) => {
     }, []);
   
   
+    const renderNewClientRegister = () => {
+      return (
+        <div style={{
+          paddingLeft: '10px'
+        }}>
+          <div>
+            <p style={{
+              fontWeight: 'bold',
+              fontSize: 'medium'
+            }}>Nie masz konta? <br />
+            </p>
+            <p className="register-welcome-text">
+              Zarejestruj się i otrzymaj dostęp do wielu udogodnień! <br />
+              Zarejestrowani użytkownicy mają dostęp do wielu udogodnień:  <br />
+            </p>
+            <ul>
+              <li>15% rabatu na nowości przy pierwszych zakupach <br /></li>
+              <li>Atrakcyjny system zniżek lojalnościowych <br /></li>
+              <li>  Oszczędność czasu przy składaniu zamówień <br /></li>
+              <li> Historia zamówień <br /></li>
+              <p style={{
+                paddingTop: '10px'
+              }}> I wiele więcej !</p>
+            </ul>
+          </div>
+          <div className="row">
+            <div style={{
+              display: 'flex',
+              justifyContent: 'flex-start',
+              marginRight: '20px',
+            }}>
+              <div style={{
+                marginRight: '20px'
+              }}>
+                <Button label="Załóż konto" variant="login-button" />
+              </div>
+              <Button label="Kontynuuj jako gość" variant="login-button" />
+            </div>
+          </div>
+        </div>
+      );
+    }
+
     return (
       <div>
-      <PasswordCredentialsContainer>
-      </PasswordCredentialsContainer>
+        <div className="content-container"
+          style={{
+            paddingTop: '140px'
+          }}>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="login-label">Logowanie</div>
+              <div className="login-description">
+                Jeśli posiadasz już konto, zaloguj się przy użyciu adresu e-mail.
+              </div>
+              <div className="column">
+                <PasswordCredentialsContainer/>
+              </div>
+            </div>
+            <div className="col-md-6">
+              {renderNewClientRegister()}
+            </div>
+          </div>
+        </div>
+        <div>
+        </div>
       </div>
     );
   };
--- src/containers/PasswordCredentialsContainer.js
+++ src/containers/PasswordCredentialsContainer.js
@@ -5,56 +5,47 @@ import { Button } from "primereact/button";
 
 
 const PasswordCredentialsContainer = (props) => {
-    const [password, setPassword] = useState(undefined);
-    const [login, setLogin] = useState(undefined);
+  const [password, setPassword] = useState(undefined);
+  const [login, setLogin] = useState(undefined);
 
-
-    const renderNewClientRegister = ()=>{
-        return (
-            <div>   
-                Nie masz konta? 
-                Zarejestruj sie i otrzymaj dostep do wielu udogodnien!
-                Zarejestrowani uztkownicy maja mozliwosc:
-                * 15% rabatu na nowości przy pierwszych zakupach
-                * Atrakcyjny system zniżek lojalnościowych
-                * Oszczędność czasu przy składaniu zamówieni
-                * Historia zamowien
-                I wiele wiecej ! 
-            </div>
-
-        );
-    }
-    return (
-     <div>
-        <div className="login label">Logowanie</div> 
-        <div className="description">
-            Jeśli posiadasz już konto, zaloguj się przy użyciu adresu e-mail.</div>
-        
-    <InputText
-     value={login} 
-     onChange={(e) => setLogin(e.target.value)} 
-     />
-    <Password
-       value={password}
-        onChange={(e) => setPassword(e.target.value)} 
-        feedback={false} 
-        tabIndex={1}
+  return (
+    <div className="column">
+      <label>E-mail</label>
+      <InputText
+        value={login}
+        onChange={(e) => setLogin(e.target.value)}
+        style={{
+          width: '100%'
+        }}
       />
+      <label>Haslo</label>
+      <Password
+        value={password}
+        className="p-password"
+        onChange={(e) => setPassword(e.target.value)}
+        feedback={false}
+        tabIndex={1}
+        inputStyle={{ width: "100%" }}
+      />
+      <div className="row">
+        <div style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          marginRight: '20px',
+        }}>
+          <a href="" style={{
+            marginRight: 'auto'
+          }}>
+            <span>Nie pamiętasz hasła?</span>
+          </a>
+          <Button label="Zaloguj" variant="login-button" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-      <div>
-      <div >
-                                            <Button
-                                                variant='forgot-password-button'
-                                            />
-                                        </div>
-                                            <Button
-                                            label='Zaloguj'
-                                            variant='login-button'
-                                        />
-                                    </div>
+export default PasswordCredentialsContainer;
 
-     </div>
-    );
-  };
-  
-  export default PasswordCredentialsContainer;
+
+
