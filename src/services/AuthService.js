fd39526f359ac77c210c0749fc74123ac744ed8a
Jakub <k.swiercz@protonmail.com>
2024-08-09 23:21:02 +0200

    added sucess login message, fixed auth service response

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -20,7 +20,7 @@ export default class AuthService{
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            return response.json();
+            return response;
         });
 
     }
