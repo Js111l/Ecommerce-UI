679f1b1e01bb1db970ec608db109d026bd794b87
Jakub <k.swiercz@protonmail.com>
2024-10-20 15:05:18 +0200

    created about us and footer element

--- src/components/ProductsShowcaseComponent.js
+++ src/components/ProductsShowcaseComponent.js
@@ -8,11 +8,7 @@ import '../App.css'
 import Toggle from 'react-toggle';
 import "react-toggle/style.css" // for ES6 modules
 import ProductService from '../services/ProductService';
-import { Button } from 'primereact/button';
-import { Tag } from 'primereact/tag';
-import { InputText } from 'primereact/inputtext';
 import { Card } from 'primereact/card';
-import PasswordCredentialsContainer from '../containers/PasswordCredentialsContainer';
 import NewsLetterComponent from './NewsLetterComponent';
 
 
@@ -22,6 +18,8 @@ const ProductsShowcaseComponent = (props) => {
   const [toggle, setToggle] = useState(false);
   const [mainBanerItems, setMainBanerItems] = useState([])
   const service = new ProductService();
+  const [hovered, setHovered] = useState({})
+  const [footerHoover, setFooterHoover] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -142,118 +140,80 @@ const ProductsShowcaseComponent = (props) => {
             }}
           />
         </div>
-        {/* <div className='container'>
-          <div className='row' style={{ display: 'flex', justifyContent: 'center', marginTop: '10%' }}>
-            {product.name}
-          </div>
-          <div className='row' style={{ display: 'flex', justifyContent: 'center', marginTop: '5%', color: 'gray' }}>
-            {product.category}
-          </div>
-          <div className='row' style={{ display: 'flex', justifyContent: 'center', marginTop: '5%', color: 'gray' }}>
-            {product.price}
-          </div>
-        </div> */}
+
       </div>
     );
   };
+  const divider = (
+    (
+      <hr style={{
+        marginTop: '3%',
+        marginBottom: '3%'
+      }} class="solid">
+      </hr>
+    )
+  )
+  const items = [
+    "Wymiana i zwroty",
+    "Metody płatności",
+    "Reklamacje",
+    "Karta podarunkowa",
+    "Konto klienta",
+    "Lista życzeń"
+  ];
+  const items2 = [
+    "Regulamin",
+    "Polityka prywatności",
+    "Pliki cookies",
+    "FAQ"
+  ];
+
+  const genderItems = [
+    "Kobieta",
+    "Mężczyzna",
+    "Dziewczynka",
+    "Chłopiec"
+  ];
+
 
   return (
-    <div
-      style={{
-        marginLeft: '15%',
-        marginRight: '15%',
-        marginTop: '3%'
-      }}
-    >
-      <div className='row'>
-        <div> //BANER 1
-          <div>
-            <Carousel value={allProducts.slice(0, 4)} numVisible={1} numScroll={1} responsiveOptions={responsiveOptions2} itemTemplate={(x) => itemTemplate(x, x.id)} />
-          </div>
-        </div>
-      </div>
-      <div>
-        {/* BESTSELLERS/ NEW PRODUCTS */}
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'flex-start',
-            marginLeft: '100px',
-            marginTop: '5%'
-          }}
-        >
-          <span style={{ marginRight: '20px' }}>Bestsellery</span>
-          <Toggle
-            defaultChecked={toggle}
-            icons={false}
-            onChange={() => {
-              setToggle(!toggle)
-            }} />
-          <span style={{ marginLeft: '20px' }}>Nowości</span>
-        </div>
-        <Carousel value={allProducts} numVisible={4} numScroll={3} responsiveOptions={responsiveOptions} itemTemplate={(x) => productTemplate(x, x.id)} />
-      </div>
-      <div>
-        <Carousel value={allProducts} numVisible={4} numScroll={3} responsiveOptions={responsiveOptions} itemTemplate={(x) => productTemplate(x, x.id)} />
-      </div>
-        {/* <div className='col-md-6'>
-          <div className='row'>
-            <div className='col-md-4'>
-              <Card
-                className="col"
-                style={{
-                  width: "100%",
-                  border: "1px solid #ccc", // Border to distinguish
-                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // Subtle shadow
-                  borderRadius: "8px", // Rounded corners
-                  padding: "10px", // Padding inside the card
-                  backgroundColor: "#f9f9f9",
-                  height:'350px'
-                }} />
-            </div>
-            <div className='col-md-4'>
-              <Card
-                className="col"
-                style={{
-                  width: "100%",
-                  border: "1px solid #ccc", // Border to distinguish
-                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // Subtle shadow
-                  borderRadius: "8px", // Rounded corners
-                  padding: "10px", // Padding inside the card
-                  backgroundColor: "#f9f9f9", // Light background color
-                  height:'350px'
-                }} />
+    <div>
+      <div
+        style={{
+          marginLeft: '15%',
+          marginRight: '15%',
+        }}
+      >
+        <div className='row'>
+          <div> 
+            <div>
+              <Carousel value={allProducts.slice(0, 4)} numVisible={1} numScroll={1} responsiveOptions={responsiveOptions2} itemTemplate={(x) => itemTemplate(x, x.id)} />
             </div>
           </div>
-          <div className='row' style={{
-            marginTop:'3%'
-          }}>
-            <div className='col-md-6'>
-              <Card
-                className="col"
-                style={{
-                  width: "100%",
-                  border: "1px solid #ccc", // Border to distinguish
-                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // Subtle shadow
-                  borderRadius: "8px", // Rounded corners
-                  padding: "10px", // Padding inside the card
-                  backgroundColor: "#f9f9f9",
-                  height:'350px'
-                }} />
-            </div>
-            <div className='col-md-6'>
-              <Card
-                className="col"
-                style={{
-                  width: "100%",
-                  border: "1px solid #ccc", // Border to distinguish
-                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // Subtle shadow
-                  borderRadius: "8px", // Rounded corners
-                  padding: "10px", // Padding inside the card
-                  backgroundColor: "#f9f9f9",
-                  height:'350px'
-                }} />
-            </div> */}
+        </div>
+        <div>
+          {/* BESTSELLERS/ NEW PRODUCTS */}
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'flex-start',
+              marginLeft: '100px',
+            }}
+          >
+            <span style={{ marginRight: '20px' }}>Bestsellery</span>
+            <Toggle
+              defaultChecked={toggle}
+              icons={false}
+              onChange={() => {
+                setToggle(!toggle)
+              }} />
+            <span style={{ marginLeft: '20px' }}>Nowości</span>
+          </div>
+          <Carousel value={allProducts} numVisible={4} numScroll={3} responsiveOptions={responsiveOptions} itemTemplate={(x) => productTemplate(x, x.id)} />
+        </div>
+        <div>
+          <Carousel value={allProducts} numVisible={4} numScroll={3} responsiveOptions={responsiveOptions} itemTemplate={(x) => productTemplate(x, x.id)} />
+        </div>
         <div className='col'
           style={{
             display: 'flex',
@@ -306,25 +266,171 @@ const ProductsShowcaseComponent = (props) => {
             }} />
 
         </div>
-        <div className='row' style={{
-          marginTop: '10%',
-          display: 'flex',
-          justifyContent: 'center'
-        }}>
-          {/* <div className="login-description">
-            Jeśli posiadasz już konto, zaloguj się przy użyciu adresu e-mail.
-          </div> */}
-          
-            <NewsLetterComponent
-              setLoading={props.setLoading}
-              showMessage={props.showMessage}
-            />
-        </div>
+      </div>
+      {divider}
+      <div className='row' style={{
+        display: 'flex',
+        justifyContent: 'center',
+        maxHeight: "300px",
+        marginLeft: '15%',
+        marginRight: '15%',
+      }}>
+        <div className="col"
+          style={{
+            backgroundColor: "#f9f9f9",
+          }}>
+          <div className='row' style={{
+            // border: '1px solid',
+            // borderColor: 'black'
+          }}>
+            <span style={{
+              fontWeight: 'bold',
+              marginBottom: '1%'
+            }}>O nas</span>
+          </div>
+          <div className='row' style={{
+            // border:'1px solid',
+            // borderColor: 'black',
+            // borderTop:'none'
+          }}>
+            <p className="m-0">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 
-        <div>
-              //FOOTER KONTAKT, LINKI DO SOCIALI OBSLUGA KLIENTA, INFORMACJE, ITP.
+            </p>
+          </div>
+        </div>
+        <div className="col">
+          <NewsLetterComponent
+            setLoading={props.setLoading}
+            showMessage={props.showMessage}
+          />
         </div>
       </div>
+      {divider}
+      <div style={{
+        marginLeft: '15%',
+        marginRight: '15%',
+      }}>
+        <div className='row'>
+          <div className='col-md-3'>
+            <ul>
+              <span>Obsługa klienta</span>
+              {items.map((x, index) => {
+                return (
+                  <div
+                    onMouseEnter={(e) => {
+                      setFooterHoover({
+                        id: index,
+                        colIndex: 0,
+                        status: true
+                      })
+                    }}
+                    onMouseLeave={(e) => {
+                      setFooterHoover({
+                        id: index,
+                        colIndex: 0,
+                        status: false
+                      })
+                    }}
+                  >
+                    <li>
+                      <span style={{
+                        textDecoration: footerHoover.id === index &&
+                          footerHoover.colIndex === 0 &&
+                          footerHoover.status
+                          ? 'underline' : '',
+                        cursor: 'pointer'
+                      }}
+                      >
+                        {x}
+                      </span>
+                    </li>
+                  </div>
+                )
+              })}
+            </ul>
+          </div>
+          <div className='col-md-3'>
+            <ul>
+              <span>Informacje</span>
+              {items2.map((x, index) => {
+                return (
+                  <div
+                    onMouseEnter={(e) => {
+                      setFooterHoover({
+                        id: index,
+                        colIndex: 1,
+                        status: true
+                      })
+                    }}
+                    onMouseLeave={(e) => {
+                      setFooterHoover({
+                        id: index,
+                        colIndex: 1,
+                        status: false
+                      })
+                    }}
+                  >
+                    <li>
+                      <span style={{
+                        textDecoration: footerHoover.id === index &&
+                          footerHoover.colIndex === 1 &&
+                          footerHoover.status
+                          ? 'underline' : '',
+                        cursor: 'pointer'
+                      }}
+                      >
+                        {x}
+                      </span>
+                    </li>
+                  </div>
+                )
+              })}
+            </ul>
+          </div>
+          <div className='col-md-3'>
+            <ul>
+              <span>Kupuj</span>
+              {genderItems.map((x, index) => {
+                return (
+                  <div
+                    onMouseEnter={(e) => {
+                      setFooterHoover({
+                        id: index,
+                        colIndex: 2,
+                        status: true
+                      })
+                    }}
+                    onMouseLeave={(e) => {
+                      setFooterHoover({
+                        id: index,
+                        colIndex: 2,
+                        status: false
+                      })
+                    }}
+                  >
+                    <li>
+                      <span style={{
+                        textDecoration: footerHoover.id === index &&
+                          footerHoover.colIndex === 2 &&
+                          footerHoover.status
+                          ? 'underline' : '',
+                        cursor: 'pointer'
+                      }}
+                      >
+                        {x}
+                      </span>
+                    </li>
+                  </div>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </div>
   );
 };
 
